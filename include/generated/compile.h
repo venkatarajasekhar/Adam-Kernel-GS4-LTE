c786d6cf02ddf2ba536989df9694a899c74de8c2
Mohamed Karami <karamimed@gmail.com>
2013-06-21 15:37:02 +0100

    Samsung Sources Update 2

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Thu May 30 16:58:57 WEST 2013"
+#define LINUX_COMPILE_BY "wanam"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.0-Adam-1.3.2+"
