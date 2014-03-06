d519a3fc905518fc205c04a99f61326447e7afe6
Mohamed Karami <karamimed@gmail.com>
2014-03-06 16:03:42 +0000

    config dump 2.1

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Fri Feb 28 14:02:53 GMT 2014"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Mar 6 10:33:30 GMT 2014"
 #define LINUX_COMPILE_BY "wanam"
 #define LINUX_COMPILE_HOST "elksiba"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-Adam-2.0"
+#define UTS_RELEASE "3.4.0-Adam-2.1"
