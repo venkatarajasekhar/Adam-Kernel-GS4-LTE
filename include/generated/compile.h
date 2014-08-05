a3efa94e16162b9233766dab0efd287dc489ede4
Mohamed Karami <karamimed@gmail.com>
2014-08-05 17:11:18 +0100

    Merge with Samsung sources update8

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Sun Mar 30 20:43:13 WEST 2014"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Aug 5 16:21:39 BST 2014"
 #define LINUX_COMPILE_BY "wanam"
 #define LINUX_COMPILE_HOST "elksiba"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-Adam-2.3"
+#define UTS_RELEASE "3.4.0-Adam-2.4"
